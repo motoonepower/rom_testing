daedd7d99ee2085be909513cfda57591a79b158c
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-05-08 09:35:21 +0530

    Update change.sh

--- change.sh
+++ change.sh
@@ -7,3 +7,6 @@ export DEVICE_BRANCH="ppui"
 export DEVICE_LUNCH="aosp_chef-userdebug"
 export BUILD_FLAG0="mka"
 export BUILD_FLAG1="bacon"
+export CUSTOM_BUILD_TYPE=OFFICIAL
+export PPUI_MAINTAINER=rushiranpise
+export IS_PHONE=true
