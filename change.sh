7c4d9ae94cd4572cf9c133934be06e5d75100863
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-05-17 11:37:50 +0530

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd /tmp/rom
 
-export ROM_LINK="https://github.com/DotOS/manifest"
-export ROM_BRANCH="dot12.1"
-export DEVICE_BRANCH="dotlos"
-export DEVICE_LUNCH="dot_chef-userdebug"
+export ROM_LINK="https://github.com/PixelPlusUI-SnowCone/manifest"
+export ROM_BRANCH="snowcone-12.1"
+export DEVICE_BRANCH="ppui"
+export DEVICE_LUNCH="aosp_chef-userdebug"
 export BUILD_FLAG0="mka"
-export BUILD_FLAG1="evolution"
+export BUILD_FLAG1="bacon"
+export CUSTOM_BUILD_TYPE=OFFICIAL
+export PPUI_MAINTAINER=rushiranpise
+export IS_PHONE=true
