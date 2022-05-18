5275610cd277e596d2d3bf6db8153abd1fc1ffcc
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-05-18 19:16:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ source $CONFIG
 cd /tmp/rom
 source build/envsetup.sh
 lunch $DEVICE_LUNCH
-$BUILD_FLAG0 sepolicy && $BUILD_FLAG0 bootimage && $BUILD_FLAG0 $BUILD_FLAG1
+#$BUILD_FLAG0 sepolicy && 
+$BUILD_FLAG0 bootimage && $BUILD_FLAG0 $BUILD_FLAG1
